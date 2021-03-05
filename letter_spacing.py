70c05093c5f15393a7f3e7a22fdf3d00ee38832e
Ankur Shah <ankur@codeplatoon.org>
2021-03-05 13:36:17 -0600

    comments

--- letter_spacing.py
+++ letter_spacing.py
@@ -1,3 +1,7 @@
+# Create an algorithm that can detect if an input string has two letters that are separated by EXACTLY N characters, and returns the two indices if it finds such a case. Otherwise, return None.
+
+# Example: If given and input string of "My name is Earl", two input characters "a" and "s", and a numeric value of "4" ... your function should return "[4,9]" because "a" and "s" are separated by EXACTLY 4 characters somewhere in the input string (index 4 "a" & index 9 "s")
+
 def letter_spacing(input_string, first_char, second_char, desired_spacing_value):
     pass
 
--- wardrobe.py
+++ wardrobe.py
@@ -1,2 +1,6 @@
+# We would like for you to design a wardrobe class, where you have a wardrobe that can store various clothing items. The wardrobe should have the ability to add new items manually, remove items manually, but also have the ability to remove all items that are a particular color (e.g. "red"), or remove all items that are a particular style (e.g. "shirt") . Finally the wardrobe should have a max capacity, which would prevent new items from being added if at max capacity already.
+
+# Clothing items should come in different varieties (shirts, sweaters, pants, shorts, belts, etc...) and colors (blue, green, red, etc..).
+
 class Wardrobe:
     pass
